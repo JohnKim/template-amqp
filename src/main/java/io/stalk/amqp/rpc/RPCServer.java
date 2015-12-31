c87bdef713cb9eec1261d9b3acec083c2d9d3098
John Kim <yohany@gmail.com>
2015-12-31 17:55:23 +0900

    add logback (for logging)

--- src/main/java/io/stalk/amqp/rpc/RPCServer.java
+++ src/main/java/io/stalk/amqp/rpc/RPCServer.java
@@ -5,16 +5,28 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.QueueingConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public abstract class RPCServer<IN extends RPCSerializable, OUT extends RPCSerializable> {
+
+    public static Logger logger = LoggerFactory.getLogger(RPCServer.class);
+
+    public static String LOG = "logFileName"; // the file name for logback MDC.
 
     public void execute(String queueName) {
 
         Connection connection = null;
         Channel channel;
+
         try {
             ConnectionFactory factory = new ConnectionFactory();
             factory.setHost("localhost");
+            factory.setAutomaticRecoveryEnabled(true);
+            factory.setTopologyRecoveryEnabled(true);
 
             connection = factory.newConnection();
             channel = connection.createChannel();
@@ -26,11 +38,11 @@ public abstract class RPCServer<IN extends RPCSerializable, OUT extends RPCSeria
             QueueingConsumer consumer = new QueueingConsumer(channel);
             channel.basicConsume(queueName, false, consumer);
 
-            System.out.println(" [x] Awaiting RPC requests [" + queueName + "]");
+            logger.info(" [x] Awaiting RPC requests [" + queueName + "]");
 
             while (true) {
 
-                OUT response = null;
+                OUT response = (OUT) new RPCSerializable();
 
                 QueueingConsumer.Delivery delivery = consumer.nextDelivery();
 
@@ -45,9 +57,10 @@ public abstract class RPCServer<IN extends RPCSerializable, OUT extends RPCSeria
 
                     response = message(data);
                 } catch (Exception e) {
+
+                    e.printStackTrace();
                     System.out.println(" [.] " + e.toString());
 
-                    response = (OUT) new RPCSerializable(); // @TODO handling ShutdownSignalException (check and reconnect)
                     response.setStatus("ERR");
                     response.setError(e.getMessage());
                 } finally {
@@ -56,8 +69,13 @@ public abstract class RPCServer<IN extends RPCSerializable, OUT extends RPCSeria
                     channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        } catch (IOException ioe) {
+            logger.error(ioe.getMessage(), ioe);
+        } catch (TimeoutException te) {
+            logger.error(te.getMessage(), te);
+        } catch (InterruptedException ie) {
+            logger.error(ie.getMessage(), ie);
         } finally {
             if (connection != null) {
                 try {
